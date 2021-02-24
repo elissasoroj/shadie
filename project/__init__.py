4a195a3a4ab02fa88f2d1667dad56b1e10fec2cc
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-02-23 22:25:16 -0500

    updated init file

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 """
-`records` queries gbif for 
+`program` is a wrapper around SLiM3 that implements selection on alternating hapliod/diploid lifecycles
+and converts user-provided phylogeny into SLiM3-compatible subpopulation demography
 """
 
 __version__ = "0.0.1"
 
-from program.program import Class
+from projoect.program import Script
