91cb6dbf38c9fde264b07b6e93cc4d93ccb9cf76
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-03-20 12:53:12 -0400

    updated __init__ to make shadie installable

--- shadie/__init__.py
+++ shadie/__init__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 """
-`program` is a wrapper around SLiM3 that implements selection on alternating hapliod/diploid lifecycles
+`shadie` is a wrapper around SLiM3 that implements selection on alternating hapliod/diploid lifecycles
 and converts user-provided phylogeny into SLiM3-compatible subpopulation demography
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.3"
 
-from project.program import Script
+from shadie.shadie import Shadie
+from shadie.chromosome import Chromosome
+from shadie.demography import Demography
