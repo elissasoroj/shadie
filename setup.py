3d802250281661811c9ce21213316dc4398e79ce
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-03-20 12:50:15 -0400

    updated setup; shadie can now be installed

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ from setuptools import setup
 # build command
 setup(
     name="SHaDiE",
-    version="0.0.1",
+    version="0.0.3",
     author="Elissa Sorojsrisom",
     author_email="ess2239@columbia.edu",
     license="GPLv3",
     description="SLiM3 Wrapper Program, 'Simulating Haploid-Diploid Evolution'",
-    install_requires = ["pandas"],
+    install_requires = ["pandas", "numpy", "toyplot", "loguru", "toytree"],
     classifiers=["Programming Language :: Python :: 3"],
 )
