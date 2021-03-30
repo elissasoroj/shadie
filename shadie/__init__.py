e68e2780891b4681013a88af987ab74611143a1c
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-03-30 13:39:43 -0400

    init works for Jupyter Notebooks now

--- shadie/__init__.py
+++ shadie/__init__.py
@@ -5,8 +5,15 @@
 and converts user-provided phylogeny into SLiM3-compatible subpopulation demography
 """
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
-from shadie.shadie import Shadie
+
+from shadie.mutations import MutationType
+from shadie.mutations import MutationList
+from shadie.elements import ElementType
+from shadie.elements import ElementList
+from shadie.globals import *
+from shadie.buildchromosome import Build
 from shadie.chromosome import Chromosome
 from shadie.demography import Demography
+from shadie.shadie import Shadie
