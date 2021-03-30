dd98b62748b7ea51ec67eafdbff336c234c39a7a
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-03-30 13:40:38 -0400

    updated internal dependencies to work with JN'

--- shadie/globals.py
+++ shadie/globals.py
@@ -1,8 +1,8 @@
 #standard mutation types, with structure:
 #(name, dominance, distribution, {following depends on distribution})
 
-from mutations import MutationType
-from elements import ElementType
+from .mutations import MutationType
+from .elements import ElementType
 
 #old strings - depreciated
 S_NEUT = '("m100", 0.5, "f", 0.0)'      #neutral mutation
@@ -18,9 +18,9 @@ S_NONCOD = '("g102", c(m100), 1)'               #non-coding
 
 
 #saved as MutationType and ElementType objects
-NEUT = MutationType(0.5, "f", [0.0])		#neutral mutation
-SYN = MutationType(0.5, "f", [0.0])         #synonymous
-DEL = MutationType(0.1, "g", [-0.03, 0.2])  #deleterious
+NEUT = MutationType(0.5, "f", 0.0)		#neutral mutation
+SYN = MutationType(0.5, "f", 0.0)         #synonymous
+DEL = MutationType(0.1, "g", -0.03, 0.2)  #deleterious
 BEN = MutationType(0.8, "e", 0.1)          	#beneficial
 
 EXON = ElementType([SYN, DEL, BEN], (2,8,0.1))  #exon
