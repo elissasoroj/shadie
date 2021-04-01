1f7bbb976549920bd56a20860c7b7d215c7bb6b9
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-04-01 14:15:26 -0400

    updated mutation distribution plotting

--- shadie/mutations.py
+++ shadie/mutations.py
@@ -99,58 +99,42 @@ class MutationType:
             f"distribution parameters: {self.distparams}\n"
             "Distribution plot:"
         )
-        if self.dist == "n":
+        if self.dist == "n": #distparams= (mean, stddev)
             mean = self.distparams[0]
             stddev = self.distparams[1]
-            source = pd.DataFrame({"Normal Distribution": np.random.normal(self.distparams[0], self.distparams[1], 5000)})
-            base = alt.Chart(source).transform_fold(
-                ["Normal Distribution"],
-                as_=['Mutation', 'Fitness Effect'])
-            histo = base.mark_bar(
-                opacity=0.3,
-            ).encode(
-                alt.X('Fitness Effect:Q', bin=alt.Bin(maxbins=100)),
-                alt.Y('count()', stack=None))
-            rule = base.mark_rule().encode(
-                x='mean(Fitness Effect):Q',
-                size=alt.value(2))
+            draws = np.random.normal(self.distparams[0], self.distparams[1], size=10000)
         elif self.dist == "g": #distparams: (mean, shape)
-            if self.dist == "g":
-                mean = self.distparams[0]
-                shape = self.distparams[1]
-                scale = mean/shape
-                stddev = np.sqrt((mean**2)/shape)
-                if mean < 0:
-                    source = pd.DataFrame({f"{self.name}": -(np.random.gamma(shape, -scale, 5000))})
-                elif mean > 0:
-                    source = pd.DataFrame({f"{self.name}": np.random.gamma(shape, scale, 5000)})
-            base = alt.Chart(source).transform_fold(
-                [f"{self.name}"],
-                as_=[f'{self.name}', 'Fitness Effect'])
-            histo = base.mark_bar(
-                opacity=0.3,
-            ).encode(
-                alt.X('Fitness Effect:Q', bin=alt.Bin(maxbins=100)),
-                alt.Y('count()', stack=None))
-            rule = base.mark_rule().encode(
-                x='mean(Fitness Effect):Q',
-                size=alt.value(2))
+            mean = self.distparams[0]
+            shape = self.distparams[1]
+            scale = mean/shape
+            stddev = np.sqrt((mean**2)/shape)
+            if mean < 0:
+                draws = -(np.random.gamma(shape, -scale, 10000))
+            elif mean > 0:
+                draws = (np.random.gamma(shape, -scale, 10000))
         elif self.dist == "w": #distparams= (scale, shape)
-            mean = "NaN"
+            shape = self.distparams[1]
+            scale = self.distparams[0]
+            mean = scale*((shape-1)/shape)**(1/shape)
             stddev = "Nan"
-            source = pd.DataFrame({"Weibull Distribution": self.distparams[0]*(np.random.weibull(self.distparams[1], 5000))})
-            base = alt.Chart(source).transform_fold(
-                ["Weibull Distribution"],
-                as_=['Mutation', 'Fitness Effect'])
-            histo = base.mark_bar(
-                opacity=0.3,
-            ).encode(
-                alt.X('Fitness Effect:Q', bin=alt.Bin(maxbins=100)),
-                alt.Y('count()', stack=None))
-            rule = base.mark_rule().encode(
-                x='mean(Fitness Effect):Q',
-                size=alt.value(2))
-        IPython.display.display_html(histo)
+            draws = self.distparams[0]*(np.random.weibull(self.distparams[1], 10000))
+        dist = np.histogram(draws, 50)
+        source = pd.DataFrame({'counts': dist[0], 'values': dist[1][:-1], 'mean':mean}, columns=['values', 'counts', 'mean'])
+
+        base = alt.Chart(source)
+        histo = base.mark_bar(
+            opacity=0.4,
+            color = alt.ColorName("cornflowerblue")
+        ).encode(
+            alt.X('values:Q', axis=alt.Axis(title='Fitness Effect')),
+            alt.Y('counts:Q'),
+        )
+        mean = base.mark_rule(color = alt.ColorName("mediumvioletred")).encode(
+            x='mean:Q',
+            size=alt.value(3),
+            tooltip=['mean:Q']
+        )    
+        IPython.display.display_html(histo + mean)
         print(
             f"mean: {mean}\n"
             f"standard deviation: {stddev}")
