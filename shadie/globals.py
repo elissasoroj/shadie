35108edc4ec40c252a0e3e27602ddc7b8776996c
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-04-10 23:02:06 -0400

    removed neutral mutations for coalescent method instead

--- shadie/globals.py
+++ shadie/globals.py
@@ -6,13 +6,13 @@ from shadie.elements import ElementType
 
 #saved as MutationType and ElementType objects
 NEUT = MutationType(0.5, "f", 0.0)		#neutral mutation
-SYN = MutationType(0.5, "f", 0.0)         #synonymous
+SYN = MutationType(0.5, "f", 0.0)         #synonymous (REMOVED for now)
 DEL = MutationType(0.1, "g", -0.03, 0.2)  #deleterious
 BEN = MutationType(0.8, "e", 0.1)          	#beneficial
 
-EXON = ElementType([SYN, DEL, BEN], (2,8,0.1))  #exon
-INTRON = ElementType([SYN,DEL], (9,1))         #intron
-NONCOD = ElementType(NEUT, 1)              	 #non-coding
+EXON = ElementType([DEL, BEN], (8,0.1))  #exon
+INTRON = ElementType([DEL], (1))         #intron
+NONCOD = ElementType(NEUT, 1, mutationrate = 0)              	 #non-coding
 
 #nucleotide defulats
 
@@ -20,5 +20,11 @@ nucEXON = ElementType([SYN, DEL, BEN], (2,8,0.1))
 
 if __name__ == "__main__":
 
-    # generate random chromosome
+    # testing
     print(EXON)
+    print(INTRON)
+    print(NONCOD)
+
+    from shadie.elements import ElementList
+    test = ElementList(EXON, INTRON, NONCOD)
+    print(test)
