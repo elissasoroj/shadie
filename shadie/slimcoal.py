72c95edbf158a65ea511281dd1a05bc42dfbf746
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-04-10 23:03:39 -0400

    adjusted code to be compatible with other modules

--- shadie/slimcoal.py
+++ shadie/slimcoal.py
@@ -9,14 +9,19 @@ Generates script for SLiM simulation
 import msprime
 import pyslim
 
-class Coal(object):
-	"""
+#internal imports
+
+class Coal:
+    """
     Overlay neutral mutations using pyslim
     """
     def __init__(
         self,
-        shadie,
+        ts,
         ):
+
+        self.ts = ts
+
         """
         Builds script to run SLiM3 simulation
 
@@ -25,10 +30,9 @@ class Coal(object):
         shadie: (Shadie class object)
             Reads in Shadie class object
         """
-        self.ts = shadie.ts
 
-	def slimcoal():
-	self.tscoal = pyslim.SlimTreeSequence(msprime.mutate(self.ts, rate=1e-6, keep=True))
+    def slimcoal(self):
+        self.tscoal = pyslim.SlimTreeSequence(msprime.mutate(self.ts, rate=1e-6, keep=True))
 
-	print(f"The tree sequence now has {tscoal.num_mutations} mutations, "
-	      f"and mean pairwise nucleotide diversity is {tscoal.diversity()}.")
+        print(f"The tree sequence now has {self.tscoal.num_mutations} mutations, "
+          f"and mean pairwise nucleotide diversity is {self.tscoal.diversity()}.")
