c394fa09b6970754efcd81a220942f0b57cbc14a
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-07-24 12:15:38 -0400

    fixed typo

--- shadie/reproduction/scripts.py
+++ shadie/reproduction/scripts.py
@@ -4,8 +4,6 @@
 defaults shadiescripts
 """
 
-from typing import Union
-
 # 1 early()
 SHADIE_POPS = """
 	sim.addSubpop('p1', dK); //diploid sporophytes
@@ -14,53 +12,68 @@ SHADIE_POPS = """
 """
 
 #early ()
-EARLY_BRYO_DIO = """
+EARLY = """
 {early()
 {{
 	if (sim.generation % 2 == 0) //diploids (p1) just generated haploid gametophytes
 	{{
 		//fitness affects gametophyte survival
 		p0.fitnessScaling = (hK / p0.individualCount);
-		
-		// haploids get modified fitness, without dominance
+
+		//p0 and p1 survival callbacks
 		s1.active = 1;
-		s2.active = 1;
+		s2.active = 0;
 		s3.active = 1;
 		s4.active = 0;
-		s5.active = 1;
-		s6.active = 0;
+
+		// haploids get modified fitness, without dominance
+		{activate}
 	}}
 	else //odd generations = gametophytes (p0) just generated sporophytes
 	{{
 		p1.fitnessScaling = dK / p1.individualCount; //fitness affects sporophytes
-		// diploids get SLiM's standard fitness calculation, with dominance
+
+		//turn off p0 survival callbacks
+		//turn on p1 survival callbacks
 		s1.active = 0;
-		s2.active = 0;
+		s2.active = 1;
 		s3.active = 0;
 		s4.active = 1;
-		s5.active = 0;
-		s6.active = 1;
-	}}
+
+		// diploids get SLiM's standard fitness calculation, with dominance
+		{deactivate}
 }}
 
 }
 """
 
-SURV_BRYO ="""
-{s3 survival(p1)
-{{
-	return F;
-}}
+#-----------------------------------------------
+#FITNESS CALLBACKS
 
-s4 survival(p1)
-{{
-	//this code implements random death chance
+#callback
+FIT = """
+return 1 + mut.selectionCoeff; //gametophytes have no dominance effects
+"""
+
+#activate/deactivate
+ACTIVATE = "{idx}.active = 1;"
+
+DEACTIVATE = "{idx}.active = 0;"
+
+#-----------------------------------------------
+#SURVIVAL CALLBACKS
+#standard defaults
+
+DEATH_CHANCE = """
+//this code implements random death chance
 	if (runif(1) < Death_chance)
 		return F;
 	else
 		return NULL;
-	
-	// maternal effect
+"""
+
+MATERNAL_EFFECT = """
+// maternal effect
 	maternal_effect = individual.getValue("maternal_fitness");
 	
 	if (!isNULL(maternal_effect))
@@ -70,9 +83,33 @@ s4 survival(p1)
 	}}
 	
 	return NULL;
+"""
+
+DEATH = """
+return F;
+"""
+
+#------
+#SHADIE SURVIVAL BLOCKS
+
+SURV ="""
+{s1 survival(p1)
+{{
+	return F;
 }}
 
-s5 survival(p0) //even
+s2 survival(p1)
+{{
+	//this code implements random death chance
+	if (runif(1) < Death_chance)
+		return F;
+	else
+		return NULL;
+	
+	{maternal_effect}
+}}
+
+s3 survival(p0) //even
 {{
 	//this code implements random death chance
 	if (runif(1) < Death_chance)
@@ -81,47 +118,14 @@ s5 survival(p0) //even
 		return NULL;
 }}
 
-s6 survival(p0) //odd
+s4 survival(p0) //odd
 {{
 	return F;
 }}
 }
 """
 
-#no maternal effect in angio, BUT we could use same script above 
-#with a default maternal_weight = 0 for angiosperm repro instead
-
-SURV_ANGIO ="""
-{s3 survival(p1)
-{{
-	return F;
-}}
-
-s4 survival(p1)
-{{
-	//this code implements random death chance
-	if (runif(1) < Death_chance)
-		return F;
-	else
-		return NULL;
-}}
-
-s5 survival(p0) //even
-{{
-	//this code implements random death chance
-	if (runif(1) < Death_chance)
-		return F;
-	else
-		return NULL;
-}}
-
-s6 survival(p0) //odd
-{{
-	return F;
-}}
-}
-"""
-
+#-----------------------------------------------
 #late() **for every mut!
 LATE = """
 {{
