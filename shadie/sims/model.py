a6f51877ae34c4b528c3d2adb3f99f276181329d
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-07-24 12:42:41 -0400

    upd ated

--- shadie/sims/model.py
+++ shadie/sims/model.py
@@ -412,13 +412,10 @@ if __name__ == "__main__":
             (3001, 5000): e1,
         })
 
-        print(chrom.data.iloc[:, :6,])
-
         # init the model
         model.initialize(chromosome=chrom)
         
         # add reproduction 
-
         # model.reproduction()
         model.early(1000, "sim.addSubpop('p1', 1000); //diploid sporophytes")
         model.fitness("m4", "return 1 + mut.selectionCoeff; //gametophytes have no dominance effects", "s1" )
