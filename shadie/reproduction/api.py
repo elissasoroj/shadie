7d80996621f1cde09481ca24948d41a7efc48c73
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-07-28 14:39:18 -0400

    added Pteridophyte model to reproduction API

--- shadie/reproduction/api.py
+++ shadie/reproduction/api.py
@@ -8,6 +8,7 @@ histories into SLiM scripts using the shadie Model context.
 
 from shadie.reproduction.base import Bryophyte
 from shadie.reproduction.angiobase import Angiosperm
+from shadie.reproduction.fernbase import Pteridophyte
 
 
 
@@ -33,7 +34,7 @@ class ReproductionApi:
         haploid_ne: int,
         female_to_male_ratio: float=0.5,
         spores_per_sporophyte: int=100,
-        clone_rate: float=1.0,
+        clone_rate: float=0.0,
         selfing_rate: float=0.,
         maternal_effect_weight: float=0,
         random_death_chance: float=0,           
@@ -61,10 +62,40 @@ class ReproductionApi:
         ).run()
 
 
-    def pteridophyte(self, ):
+    def pteridophyte(
+        self, 
+        chromosome,
+        mode:str, 
+        diploid_ne: int,
+        haploid_ne: int,
+        spores_per_sporophyte: int=100,
+        female_to_male_ratio: float=0.5,
+        clone_rate: float=0.0,
+        selfing_rate: float=0.0,
+        maternal_effect_weight: float=0,
+        random_death_chance: float=0,       
+        ):
         """
-        TODO:
+        Generate scripts appropriate for an angiosperm (flowering plant)
+        life history. Appropriate for gymnosperms as well.
+        This adds code to the following 
+        SLiM script blocks: reproduction, early, ...
+
+        Parameters:
+        -----------
+        mode: str
+            A life history strategy or "dio" or "mono" -ecious.
+        ...
         """
+        Pteridophyte(
+            model=self.model, chromosome=chromosome, mode=mode,
+            diploid_ne=diploid_ne, haploid_ne=haploid_ne,
+            female_to_male_ratio=female_to_male_ratio,
+            spores_per_sporophyte=spores_per_sporophyte,
+            clone_rate=clone_rate, selfing_rate=selfing_rate,
+            maternal_effect_weight=maternal_effect_weight,
+            random_death_chance=random_death_chance,
+        ).run()
 
     def spermatophyte(self, ):
         """
