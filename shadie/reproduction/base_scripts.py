b5a1fbddb0c92af15989120c9992f337dbfae7cc
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-07-28 14:40:15 -0400

    cosmetic

--- shadie/reproduction/base_scripts.py
+++ shadie/reproduction/base_scripts.py
@@ -371,3 +371,51 @@ REPRO_PTER_HOMOSPORE_P1 = """
         p0.addRecombinant(g_2, g_1, breaks, NULL, NULL, NULL);
     }
 """
+
+REPRO_PTER_HETEROSPORE_P0 = """
+    if (runif(1) < gFtoM) // chance of making meristic (egg-bearing) gametophyte
+    {
+        reproduction_opportunity_count = 1;
+        
+        // clones give the focal individual extra opportunities to reproduce
+        if (runif(1) <= gClone_rate)
+            reproduction_opportunity_count = reproduction_opportunity_count + Clone_num;
+        
+        for (repro in seqLen(reproduction_opportunity_count))
+        {
+            if (runif(1) <= Self_rate)
+                p1.addRecombinant(individual.genome1, NULL, NULL, individual.genome1, NULL, NULL);
+                // this is selfing using two identical gametes – intragametophytic selfing
+                // intergametophytic selfing might happen below, by chance
+
+            else
+            {
+                sperm = p0.sampleIndividuals(1);
+                
+                child = p1.addRecombinant(individual.genome1, NULL, NULL, sperm.genome1, NULL, NULL);
+                
+                if (Maternal_weight > 0) //Mother's fitness affects sporophyte fitness; see survival()
+                    child.setValue("maternal_fitness", subpop.cachedFitness(individual.index));
+            
+            }
+        }
+    }
+"""
+
+REPRO_PTER_HETEROSPORE_P1 = """
+    g_1 = genome1;
+    g_2 = genome2;
+    
+    meiosis_reps = floor(Spore_num/2);
+    reproduction_opportunity_count = 1;
+    // clones give the focal individual extra opportunities to reproduce
+    if (runif(1) <= Clone_rate)
+        meiosis_reps = meiosis_reps*Clone_num;
+    
+    for (rep in 1:meiosis_reps)
+    {
+        breaks = sim.chromosome.drawBreakpoints(individual);
+        p0.addRecombinant(g_1, g_2, breaks, NULL, NULL, NULL);
+        p0.addRecombinant(g_2, g_1, breaks, NULL, NULL, NULL);
+    }
+"""
