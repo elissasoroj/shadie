9dfb216266dc5c4a7196342eeafdc3bf47b019fc
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-07-30 13:00:06 -0400

    added chromosome viewer functions back in

--- shadie/chromosome/build.py
+++ shadie/chromosome/build.py
@@ -9,6 +9,8 @@ import itertools
 import pandas as pd
 import numpy as np
 from loguru import logger
+import altair as alt
+import toyplot
 
 # internal imports
 from shadie.base.elements import ElementType
@@ -49,9 +51,194 @@ class ChromosomeBase:
 
     def inspect(self):
         """
-        Visualize chromosome structure using altair interactive.
+        Takes ChromosomeBase class object parameter
         """
-        return ""
+        
+        eltype = []
+        altname = []
+        startbase = []
+        endbase = []
+        y1 = []
+        y2 = []
+        length = []
+        for index, row in self.data.iterrows():
+            eltype.append(row['eltype'])
+            if pd.isna(row['name']):
+                altname.append(row['eltype'])
+            else: 
+                altname.append(row['name'])
+            startbase.append(row['start'])
+            endbase.append(row['end'])
+            y1.append(0)
+            y2.append(1)
+            length.append(row['end']-row['start'])
+
+        chromcoords = list(zip(eltype, altname, startbase, endbase, y1, y2, length))
+        genome = pd.DataFrame(chromcoords, 
+            columns = ['altname', 'eltype', 'x1', 'x2', 'y1', 'y2', 'length'])
+
+        #set the colors
+        extypecount = genome.loc[genome['eltype']=='exon'].altname.nunique()
+        extypes = list(genome.loc[genome['eltype']=='exon'].altname.unique())
+        intypecount = genome.loc[genome['eltype']=='intron'].altname.nunique()
+        intypes = list(genome.loc[genome['eltype']=='intron'].altname.unique())
+        nctypecount = genome.loc[genome['eltype']=='noncds'].altname.nunique()
+        nctypes = list(genome.loc[genome['eltype']=='noncds'].altname.unique())
+        self.intypes = intypes
+        self.extypes = extypes
+
+        ncodcolors = ['mediumvioletred', 'lightcoral','firebrick', 'crimson', 'lightpink']
+        incolors = ['lemonchiffon', 'gold', 'orange',  'yellow', 'khaki']
+        excolors = ['cornflowerblue', 'mediumblue','dodgerblue', 'darkslateblue', 'skyblue']
+
+
+        dom = extypes + intypes + nctypes
+        rng = excolors[0:extypecount] + incolors[0:intypecount] + ncodcolors[0:nctypecount]
+
+        brush = alt.selection_interval(
+            encodings=['x'], 
+            mark=alt.BrushConfig(fill='red', fillOpacity = 0.700))
+
+        #make the altair plot
+        ichrom = alt.Chart(genome).mark_rect().encode(
+            x=alt.X('x1:Q', axis=alt.Axis(title='Base Pairs')),
+            x2='x2:Q',
+            y = alt.Y('y1:Q', axis=None),
+            y2='y2:Q', 
+            color=alt.Color('altname:N', 
+                            scale=alt.Scale(domain=dom, range=rng)),
+            tooltip=[
+                alt.Tooltip('eltype', title='Element Type'),
+                alt.Tooltip('altname', title='Name'),
+                #alt.Tooltip('mutations', title='Mutations'),
+                alt.Tooltip('length', title='Length'),
+                alt.Tooltip('x1', title='Start'),
+                alt.Tooltip('x2', title='Stop'),
+                    ]
+        ).properties(
+            width = 850
+        ).add_selection(brush)
+
+        zoom = alt.vconcat(
+            ichrom.encode(
+            alt.X('x1:Q', title=None, scale=alt.Scale(domain=brush))
+            ).properties(height = 80),
+            ichrom.add_selection(brush).properties(height=40),
+            data=genome)
+
+        zoom.save('zoom.html')
+        self.ichrom = ichrom    #for postsim
+        self.zoom = zoom        #for interactive plot
+
+
+
+
+    def toyplot(self):
+        "Makes static toyplot"
+        eltype = []
+        startbase = []
+        endbase = []
+        y1 = []
+        y2 = []
+        for index, row in self.data.iterrows():
+            eltype.append(row['name'])
+            startbase.append(row['start'])
+            endbase.append(row['end'])
+            y1.append(0)
+            y2.append(1)
+
+        chromcoords = list(zip(eltype, startbase, endbase, y1, y2))
+        rectangles = pd.DataFrame(chromcoords, columns = [
+            'Element Type', 'x1', 'x2', 'y1', 'y2'])
+
+        #define colors for each element
+        color = []
+        for index, row in rectangles.iterrows():
+            if row["Element Type"] == "noncds":
+                color.append("firebrick")
+            elif row["Element Type"] == "exon":
+                color.append("steelblue")
+            elif row["Element Type"] == "intron":
+                color.append("orange")
+
+        #assign colors to rectangles dataframe
+        rectangles.insert(5, "color", color)
+
+        #Calculate Stats
+        self.genecount = max(1, (rectangles["Element Type"]=='noncds').sum()-1)
+        self.exoncount = (rectangles["Element Type"]=='exon').sum()
+        self.introncount = (rectangles["Element Type"]=='intron').sum()
+        self.totexonlength = 0
+        self.totintronlength = 0
+        for index, row in rectangles.iterrows():
+            if row["Element Type"]=='exon':
+                self.totexonlength += (1+row["x2"]-row["x1"]) 
+            elif row["Element Type"]=='intron':
+                self.totintronlength += (1+row["x2"]-row["x1"])
+
+        if self.introncount == 0:
+            self.avintron = 0
+        else:
+            self.avintron = self.totintronlength/self.introncount
+
+        self.rectangles = rectangles
+
+    def review(self, item = None):
+        """
+        allows user to inspect chromosome settings, including:
+        mutation rates, genomic element types, and genomic elements
+        also creates interactive plot of the chromosome, so the user can inspect it
+        """
+
+        if item == "mutations":
+            print('\033[1m' + "Mutation Types:\n" + '\033[0m', self.mutationlist, "\n")
+            #print("Mutations:\n" self.mutdict)
+        elif item == "eltypes":
+            print('\033[1m' + "Genomic Element Types:\n" + '\033[0m', self.elementlist, "\n")
+            #print("Genomic Element Types:\n" self.eldict)
+        elif item == "elements":
+            df = pd.DataFrame(self.genome)
+            print('\033[1m' + "Genomic Elements:\n" + '\033[0m')
+            display(df)
+
+        elif item == "chromosome":
+            self.toyplot()
+            
+            print(
+                '\033[1m' + "Chromosome Summary\n" + '\033[0m'
+                f"# of Genes: {self.genecount}\n"
+                f"Average # exons per gene: {self.exoncount/self.genecount}\n"
+                f"Average exon length: {self.totexonlength/self.exoncount} nt\n"
+                f"Average # introns per gene: {self.introncount/self.genecount}\n"
+                f"Average introns length: {self.avintron} nt\n"
+                )
+
+            print(f"Static Chromosome Plot:\n")
+            """
+            this is a simplified plot showing exons, coding regions, 
+            and non-codding regions collapsed, even if they consist of 
+            more that one genomic element type
+            """
+            # make the canvas and axes with toyplot
+            canvas = toyplot.Canvas(width=2400, height=200)
+            axes = canvas.cartesian()
+            axes.show = True
+
+            #draw the rectangles
+            for index, row in self.rectangles.iterrows():
+                axes.rectangle(
+                    row['x1'], row['x2'], row['y1'], row['y2'],
+                    color = row['color']
+                )
+
+        elif item == "interactive":
+            self.altair()
+            print("Interactive altair chromosome map:")
+            IPython.display.display_html(self.zoom)
+            
+        else:
+            logger.info("Please enter a valid item to review. Options include:\n"
+                "'mutations'\n'eltypes'\n'elements'\n''chromosome'\n'interractive'")
 
     def to_slim_mutation_types(self):
         """
@@ -322,6 +509,8 @@ if __name__ == "__main__":
 
     chrom = shadie.chromosome.random(100000)
     print(chrom.data.iloc[:50, :4])
+    chrom.review("chromosome")
+
 
     # # design chromosome of elements
     # # Do we want users to be able to put in a chromosome like this 
