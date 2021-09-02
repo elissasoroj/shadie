be016b92eb0cbee48587af62e010107f5974841c
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-09-02 07:28:01 -0400

    turned on coalescence checking

--- shadie/sims/format.py
+++ shadie/sims/format.py
@@ -16,7 +16,7 @@ initialize() {{
   // config
   initializeRecombinationRate({recombination_rate});
   initializeMutationRate({mutation_rate});
-  initializeTreeSeq();
+  initializeTreeSeq(checkCoalescence=T);
 
   // MutationType init
   {mutations}
