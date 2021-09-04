49d0e0f5934b231d30a82f7b31af7adf50ab8387
Deren <de2356@columbia.edu>
2021-09-04 12:27:06 -0400

    bryophyte superclass structure

--- shadie/reproduction/bryobase.py
+++ shadie/reproduction/bryobase.py
@@ -2,6 +2,15 @@
 
 """
 Bryophyte reproduction class is a superclass of NonWrightFisher class.
+
+Class inheritance structure
+---------------------------
+ReproductionBase
+    WrightFisher
+    NonWrightFisher
+        Brypophyte
+            BryophyteDioicous
+            BryophyteMonoicous
 """
 
 from typing import Tuple, Optional
@@ -27,63 +36,21 @@ MTYPES = ("monoicy", "monoicous", "homosporous")
 
 
 @dataclass
-class BryophyteBase(NonWrightFisher):
-    """NonWF type with lineage=Bryophyte and requiring proper mode."""
-    lineage: str = field(default="Bryophyte", init=False)
-    mode: str
-
-    def __post_init__(self):
-        # substring matching to three options.
-        if any(self.mode in i for i in DTYPES):
-            self.mode = "heterosporous"
-        elif any(self.mode in i for i in MTYPES):
-            self.mode = "homosporous"
-        else:
-            raise ValueError(
-                "mode arg must be a substring of one of these terms: "
-                f"{DTYPES + MTYPES}. You entered {self.mode}."
-            )
-
-@dataclass
-class Bryophyte(BryophyteBase):
+class Bryophyte(NonWrightFisher):
     """Reproduction mode based on mosses, hornworts, and liverworts."""
+    lineage: str = field(default="Bryophyte", init=False)
     spo_pop_size: int
     gam_pop_size: int
     spo_mutation_rate: Optional[float]=None
     gam_mutation_rate: Optional[float]=None
-    gam_female_to_male_ratio: Tuple[float,float]=(1, 1)
-    spo_spores_per: int=100
-    spo_megaspores_per: int=1
+    spo_spores_per: int=500
     gam_sporophytes_per: int=10
-    gam_clone_rate: float=0.0
-    gam_clone_number: int=1
-    gam_self_rate: float=0
-    gam_maternal_effect: float=0
-    spo_random_death_chance: float=0
-    gam_random_death_chance: float=0
-
-    def run(self):
-        """Runs subfunctions to fill self.model.map."""
-        # check of modify input params.
-        self._set_gam_female_to_male_ratio_as_float()
-        self._set_mutation_rates()
-
-        # methods inherited from parent NonWrightFisher class
-        self._define_subpopulations()
-        self._write_trees_file()
-
-        # add bryo reproduction blocks and initialize constants
-        self._add_shared_mode_scripts()
-        if self.mode == "heterosporous":
-            self._add_unique_to_heterosporous()
-        else:
-            self._add_unique_to_monosporous()
-
-    def _set_gam_female_to_male_ratio_as_float(self):
-        """Convert tuple ratio to a float."""
-        sum_ratio = sum(self.gam_female_to_male_ratio)
-        float_ratio = self.gam_female_to_male_ratio[0] / sum_ratio
-        self.gam_female_to_male_ratio = float_ratio
+    gam_clone_rate: float=0.8
+    gam_clone_number: int=100
+    gam_maternal_effect: float=0.5
+    spo_self_rate: float=0.1
+    spo_random_death_chance: float=0.08
+    gam_random_death_chance: float=0.08
 
     def _set_mutation_rates(self):
         """Set mutation rates for both, or use Model rate / 2 for both."""
@@ -103,10 +70,9 @@ class Bryophyte(BryophyteBase):
         This will define survival callback functions s5-s8 which are
         turned on or off depending on model parameters.
         """
-
         # add fitness callback for gametophytes based on MutationTypes
         # in the model.chromosome.
-        # this will map to s1-s4 survival callbacks.
+        # this will map to sx-sy survival callbacks.
         idx = 4
         activate_scripts = []
         deactivate_scripts = []
@@ -125,7 +91,7 @@ class Bryophyte(BryophyteBase):
             # it is the haploid or diploid subpopulation's generation.
             self.model.fitness(
                 idx=sidx,
-                mutation=mut,
+                mutation=mut.name,
                 scripts="return 1 + mut.selectionCoeff",
                 comment="gametophytes have no dominance effects",
             )
@@ -136,7 +102,7 @@ class Bryophyte(BryophyteBase):
 
             # add reference to this mutation to be added to a late call
             # for checking whether a mutation has become a substitution.
-            sub_inner = SUB_INNER.format(idx=sidx, mut=mut).lstrip()
+            sub_inner = SUB_INNER.format(idx=sidx, mut=mut.name).lstrip()
             substitutions.append(sub_inner)
 
         # insert references to fitness callbacks into an early script
@@ -175,20 +141,52 @@ class Bryophyte(BryophyteBase):
             comment="fixes mutations in haploid gen"
         )
 
-    def _add_unique_to_heterosporous(self):
-        """Fills model.map reproduction block with bryophyte-dioicous."""
+    def _initialize_constants(self):
+        """Add defineConstant calls to init variables.
 
-        # add defineConstant calls to init new variables.
-        # exclude parent class attributes and those not in this mode.
-        # TODO: should gam_self_rate be in this mode or not?
-        exclude = ["lineage", "mode", "model"] # "gam_self_rate"]
+        When this is called by different superclasses of Bryophyte
+        that have different attributes unique to each it stores only
+        its own unique set of attributes.
+        """
+        # exclude parent class attributes
+        exclude = ["lineage", "mode", "model"]
         asdict = {
             i: j for (i, j) in self.__dict__.items()
             if i not in exclude
         }
         self.model.map["initialize"][0]['constants'].update(asdict)
 
-        # add reproduction scripts for heterosporous
+
+@dataclass
+class BryophyteDioicous(Bryophyte):
+    mode: str = field(default="heterosporous", init=False)
+    gam_female_to_male_ratio: Tuple[float,float]=(2, 1)
+    spo_megaspores_per: int=1
+    spo_microspores_per: int=1
+
+    def run(self):
+        """Fill self.model.map with SLiM script snippets."""
+        # methods inherited from parent NonWrightFisher class
+        self._define_subpopulations()
+        self._write_trees_file()
+
+        # methods inherited from parent Bryophyte class
+        self._set_mutation_rates()
+        self._add_shared_mode_scripts()
+
+        # mode-specific functions
+        self._set_gam_female_to_male_ratio_as_float()        
+        self._initialize_constants()
+        self._add_mode_scripts()
+
+    def _set_gam_female_to_male_ratio_as_float(self):
+        """Convert tuple ratio to a float."""
+        sum_ratio = sum(self.gam_female_to_male_ratio)
+        float_ratio = self.gam_female_to_male_ratio[0] / sum_ratio
+        self.gam_female_to_male_ratio = float_ratio
+
+    def _add_mode_scripts(self):
+        """Add reproduction scripts unique to heterosporous bryo."""
         self.model.repro(
             population="p1",
             scripts=REPRO_BRYO_DIO_P1,
@@ -200,21 +198,28 @@ class Bryophyte(BryophyteBase):
             comment="generates gametes from sporophytes"
         )
 
-    def _add_unique_to_monosporous(self):
+
+@dataclass
+class BryophyteMonoicous(Bryophyte):
+    mode: str = field(default="homosporous", init=False)
+    gam_self_rate: float=0.2
+
+    def run(self):
+        """Fill self.model.map with SLiM script snippets."""
+        # methods inherited from parent NonWrightFisher class
+        self._define_subpopulations()
+        self._write_trees_file()
+
+        # methods inherited from parent Bryophyte class
+        self._set_mutation_rates()
+        self._add_shared_mode_scripts()
+
+        # mode-specific functions
+        self._initialize_constants()
+        self._add_mode_scripts()
+
+    def _add_mode_scripts(self):
         """fills the model.map block with bryophyte-monoicous scripts."""
-
-        # add defineConstant calls to init new variables.
-        # exclude parent class attributes and those not in this mode.
-        exclude = [
-            "lineage", "mode", "model",
-            "gam_female_to_male_ratio", "spo_megaspore_per"
-        ]
-        asdict = {
-            i: j for (i, j) in self.__dict__.items()
-            if i not in exclude
-        }
-        self.model.map["initialize"][0]['constants'].update(asdict)
-
         # add reproduction scripts
         self.model.repro(
             population="p1",
@@ -226,6 +231,7 @@ class Bryophyte(BryophyteBase):
             scripts=REPRO_BRYO_MONO_P0,
             comment="generates gametes from sporophytes"
         )
+
 
 
 if __name__ == "__main__":
@@ -250,8 +256,7 @@ if __name__ == "__main__":
 
     with shadie.Model() as mod:
         mod.initialize(chromosome=chrom, sim_time=50, file_out="/tmp/test.trees")
-        mod.reproduction.bryophyte(
-            mode='dio',
+        mod.reproduction.bryophyte_dioicous(
             spo_pop_size=100,
             gam_pop_size=100,
         )
