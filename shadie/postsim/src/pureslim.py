c2ca31fc03f90e2d59b21694045641ffe853e43d
Elissa Sorojsrisom <ess2239@columbia.edu>
2022-02-20 18:05:45 -0500

    fixed filepath to toytree toysequence scripts

--- shadie/postsim/src/pureslim.py
+++ shadie/postsim/src/pureslim.py
@@ -14,7 +14,7 @@ import scipy.stats
 from loguru import logger
 
 import toyplot
-from toytree.utils import ToyTreeSequence, ScrollableCanvas
+from toytree.utils import toytree_sequence, ScrollableCanvas
 from shadie.chromosome.src.classes import ChromosomeBase
 
 class PureSlim:
