6c79cb87a6381de72fcaa37d0c5a1398ba6de0ea
Elissa Sorojsrisom <ess2239@columbia.edu>
2023-04-08 15:46:24 -0400

    adding plotting function

--- shadie/postsim/__init__.py
+++ shadie/postsim/__init__.py
@@ -7,3 +7,4 @@ shadie post simulation analysis utils
 from .src.result import TwoSims
 from .src.one_sim import OneSim
 from .src.pureslim import PureSlim, PureSlim_TwoPops
+from .src.ts_utils import plot
