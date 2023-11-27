8a06d27a33174d061196a57cc0b407a86078884b
Elissa Sorojsrisom <ess2239@columbia.edu>
2023-11-26 22:06:16 -0500

    changed _neg to _sign

--- shadie/base/elements.py
+++ shadie/base/elements.py
@@ -115,7 +115,7 @@ class ElementType:
             yshow=False,
         )
         mixture = np.concatenate([
-            i._dist.rvs(**i._params, size=10000) * i._neg for i in self.mlist
+            i._dist.rvs(**i._params, size=10000) * i._sign for i in self.mlist
         ])
         weights = np.concatenate([
             np.repeat(self.freq[i], 10000) for i in range(len(self.mlist))
