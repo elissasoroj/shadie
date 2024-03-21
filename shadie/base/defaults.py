d8785b5a5263472bd5a85f6e136efba4316042f7
Deren <de2356@columbia.edu>
2024-03-21 16:36:56 -0400

    mtype new usage

--- shadie/base/defaults.py
+++ shadie/base/defaults.py
@@ -1,37 +1,27 @@
 #!/usr/bin/env python
 
-"""
-Global defaults for shadie. 
+"""Global defaults for shadie. 
 
 These default MutationTypes are used to model neutral or non-neutral 
 mutations in simulation unless the user creates additional custom 
 MutationTypes.
 """
 
-from loguru import logger
+# from loguru import logger
 from shadie.base.mutations import mtype
 from shadie.base.elements import ElementType
 
-
 # MutationTypes: selection coefficient distributions
-NEUT = mtype(0.5, 'f', 0.0, True, True)      # neutral mutation
-DEL = mtype(0.1, 'g', (-3.0, 1.5), True, True)  # deleterious
-BEN = mtype(0.8, 'e', 0.04, True, True)       # beneficial
+NEUT = mtype(0.5, 'f', 0.0)               # neutral mutation
+DEL = mtype(0.1, 'g', (-3.0, 1.5))        # deleterious
+BEN = mtype(0.8, 'e', 0.04)               # beneficial
 
 # EXON has 80X more deleterious (-Gamma underdominant) than it has
 # beneficial (+E overdominant) mutations.
-EXON = ElementType(
-	[DEL, BEN], 
-	(8, 0.1),  
-	altname="exon",
-)
+EXON = ElementType([DEL, BEN], (8, 0.1), altname="exon")
 
 # INTRON has only deleterious (-Gamma) mutations
-INTRON = ElementType(
-	[DEL], 
-	[1],  
-	altname="intron",
-	)
+INTRON = ElementType([DEL], [1], altname="intron")
 
 # NONCOD has only neutral (fixed) and no mutations recorded.
 NONCDS = ElementType([NEUT], [1], altname="noncds")
@@ -41,7 +31,6 @@ SYN = ElementType([NEUT], [1], altname="syn")
 
 #SYN is used by shadie for synonymous regiions inside coding regions
 EMPTY = ElementType([NEUT], [1], altname="empty") 
-
 
 if __name__ == "__main__":
     pass
